314678d0303907feee86cf591ada6ba7418142c4
Beatriz P Machado <beatriz.paes@jala.university>
2024-12-02 22:24:38 -0300

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/networkservicehub/NshApplication.java
@@ -0,0 +1,13 @@
+package com.networkservicehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NshApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/networkservicehub/NshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.networkservicehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NshApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
